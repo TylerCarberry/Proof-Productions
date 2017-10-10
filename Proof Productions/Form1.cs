3cad8f0bb0cf1c6fcc11d4d4df0cdc1aad4944ae
Tyler Carberry <tylercarberry@gmail.com>
2017-10-10 12:21:21 -0700

    Rename rotate buttons

--- Proof Productions/Form1.cs
+++ Proof Productions/Form1.cs
@@ -22,17 +22,17 @@ namespace Proof_Productions
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ClockwiseClick(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void CounterClockwiseClick(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void EstopButtonClick(object sender, EventArgs e)
         {
 
         }
