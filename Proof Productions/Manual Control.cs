757799378fdb7a257db81dd86ac27538c0629470
Tyler Carberry <TylerCarberry@users.noreply.github.com>
2017-10-26 12:46:48 -0700

    Create about window (#7)
    Create about window including "Proof Productions", the version number, and proofproductionsinc.com that opens the web browser when clicked.

--- Proof Productions/Manual Control.cs
+++ Proof Productions/Manual Control.cs
@@ -140,5 +140,10 @@ namespace Proof_Productions
         {
 
         }
+
+        private void aboutButton_Click(object sender, EventArgs e)
+        {
+            new AboutForm().Show();
+        }
     }
 }
