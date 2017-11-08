1a74ace4f454a662fcb39be6912ad2023c9821dd
Tyler Carberry <tylercarberry@gmail.com>
2017-11-08 08:44:26 -0800

    Add some model classes
    I'm not done yet, I still have more classes to move over

---
+++ Proof Productions/model/input/Acceleration.cs
@@ -0,0 +1,24 @@
+﻿namespace Proof_Productions.model
+{
+    class Acceleration
+    {
+
+        int acceleration;
+
+        public Acceleration()
+        {
+            acceleration = 0;
+        }
+
+        public void Set(int acceleration)
+        {
+            this.acceleration = acceleration;
+        }
+
+        public int Get()
+        {
+            return acceleration;
+        }
+
+    }
+}
---
+++ Proof Productions/model/input/SetpointVelocity.cs
@@ -0,0 +1,24 @@
+﻿namespace Proof_Productions.model
+{
+    class SetpointVelocity
+    {
+
+        int setpointVelocity;
+
+        public SetpointVelocity()
+        {
+            setpointVelocity = 0;
+        }
+
+        public void Set(int setpointVelocity)
+        {
+            this.setpointVelocity = setpointVelocity;
+        }
+
+        public int Get()
+        {
+            return setpointVelocity;
+        }
+
+    }
+}
