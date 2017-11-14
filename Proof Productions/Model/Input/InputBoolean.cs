524bbe907798970ce783de44839d20c17f7280fd
Benny <liangb6@students.rowan.edu>
2017-11-14 00:56:45 -0500

    WIP

---
+++ Proof Productions/Model/Input/InputBoolean.cs
@@ -0,0 +1,23 @@
+﻿namespace Proof_Productions.Model.Input
+{
+    public abstract class InputBoolean
+    {
+        
+        protected byte ConvertBoolArraytoByte(bool[] source)
+        {
+            byte result = 0;
+
+            int index = 8 - source.Length;
+            foreach (bool b in source)
+            {
+                if (b)
+                {
+                    result |= (byte)(1 << (index));
+                }
+                index++;
+            }
+            return result;
+        }
+        
+    }
+}
