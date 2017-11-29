d85f5641ed97db65b46f1ceec4c6ac35e6070ae6
Tyler Carberry <TylerCarberry@users.noreply.github.com>
2017-11-28 19:52:20 -0800

    Create setup cues form gui (#30)
    Create setups cues form gui and create list of motor items from GUI

--- Proof Productions/View/SetupCuesForm.cs
+++ Proof Productions/View/SetupCuesForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Proof_Productions.Controller;
+using Proof_Productions.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,36 +19,6 @@ namespace Proof_Productions.View
             InitializeComponent();
         }
 
-        private void cueSelectionBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dirComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void spdComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void accelComboBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void detailedViewTextBox_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
         /* Inserts a new row after the currently selected row in the motorDataGridView.
          * @param sender
          * @param e 
@@ -59,30 +31,6 @@ namespace Proof_Productions.View
                 motorDataGridView.Rows.Add(new DataGridViewRow());
             else
                 motorDataGridView.Rows.Insert((motorDataGridView.CurrentCell.RowIndex + 1), new DataGridViewRow());
-        }
-
-        /** Prompt user if they want to remove a row. If yes, Removes the currently selected row,
-         *  or does nothing if there is no row to remove.
-         *  @param sender
-         *  @param e
-         * */
-        private void removeRowButton_Click(object sender, EventArgs e)
-        {
-            DialogResult answer = MessageBox.Show("Are you sure?", "Remove Cue",
-                                  MessageBoxButtons.YesNo, MessageBoxIcon.Warning);           
-            if (answer == DialogResult.Yes)
-            {
-                //if nothing has ever been added to table - CurrentRow is null
-                if (motorDataGridView.CurrentRow == null || motorDataGridView.CurrentRow.IsNewRow)
-                    return;
-                else
-                {
-                    int rowIndex = motorDataGridView.CurrentRow.Index;
-                    if (rowIndex < motorDataGridView.Rows.Count)
-                        motorDataGridView.Rows.RemoveAt(rowIndex);
-                    //TODO - should we remove anything if we're selecting NewRow?
-                }
-            }
         }
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e) {
@@ -113,6 +61,89 @@ namespace Proof_Productions.View
         private void setupMotorsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             switchToForm(new SetupMotorForm());
+        }
+
+        private void saveCueButton_Click(object sender, EventArgs e)
+        {
+            Cue cue = new Cue();
+
+            foreach(DataGridViewRow row in motorDataGridView.Rows)
+            {
+
+                try
+                {
+                    String name = row.Cells[0].Value.ToString();
+                    String motorStr = row.Cells[1].Value.ToString();
+                    String startDelayStr = row.Cells[2].Value.ToString();
+                    String durationStr = row.Cells[3].Value.ToString();
+                    Boolean clockwise = row.Cells[4].Value.ToString().Equals("true");
+                    String speedStr = row.Cells[5].Value.ToString();
+                    String accelerationStr = row.Cells[6].Value.ToString();
+                    String decelerationStr = row.Cells[7].Value.ToString();
+
+                    // TODO For Tom
+                    /*
+                    if (InputValidator.IsValid(speed)
+                    // and check all of the other fields
+                    {
+
+                    }
+                    else show a dialog and don't write to the motor
+                    */
+
+                    int speed = int.Parse(speedStr);
+                    int accel = int.Parse(accelerationStr);
+                    int decel = int.Parse(decelerationStr);
+                    int startDelay = int.Parse(startDelayStr);
+                    int duration = int.Parse(durationStr);
+
+                    // TODO: Don't hardcode the IP address or description
+                    Motor motor = new Motor("192.168.10.4", name, "This is the main motor for controlling the curtain", new PLC());
+
+                    CueItem cueItem = new CueItem(startDelay, duration, motor, speed, accel, decel, clockwise, 0);
+                    cue.Add(cueItem);
+                }
+                catch (NullReferenceException exception)
+                {
+                    // TODO For Tom, show a dialog
+                    return;
+                }
+
+               
+                
+
+                
+
+            }
+
+            // TODO Save cueItems to the database
+        }
+
+        /** Prompt user if they want to remove a row. If yes, Removes the currently selected row,
+         *  or does nothing if there is no row to remove.
+         *  @param sender
+         *  @param e
+         * */
+        private void deleteCueItemButton_Click(object sender, EventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Are you sure?", "Remove Cue",
+                                  MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (answer == DialogResult.Yes)
+            {
+                //if nothing has ever been added to table - CurrentRow is null
+                if (motorDataGridView.CurrentRow == null || motorDataGridView.CurrentRow.IsNewRow)
+                    return;
+                else
+                {
+                    int rowIndex = motorDataGridView.CurrentRow.Index;
+                    if (rowIndex < motorDataGridView.Rows.Count)
+                    {
+                        motorDataGridView.Rows.RemoveAt(rowIndex);
+                        // TODO: Also remove the CueItem from the Cue
+                    }  
+                }
+
+            }
         }
     }
 }
