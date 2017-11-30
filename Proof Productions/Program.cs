d30249b973740c2b06df59483053423f07ce0ed7
caow2 <caow2@students.rowan.edu>
2017-11-30 16:48:08 -0500

    Created and tested database functions

--- Proof Productions/Program.cs
+++ Proof Productions/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Proof_Productions.Model;
 
 namespace Proof_Productions
 {
@@ -17,6 +18,16 @@ namespace Proof_Productions
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            /*testing for database
+             * TODO - delete later
+            Cue cue = new Cue();
+            DataAccess da = new DataAccess();
+            da.connect();
+            //Console.WriteLine(da.getCueID());
+            Console.WriteLine("Testing");
+            da.disconnect();
+            */
 
             //Run different Forms
             Application.Run(new MainMenuForm());
