3a31b6fe52bbd00de1669242df4251f116c92ae4
abrahamt0 <abrahamt0@students.rowan.edu>
2017-12-15 13:24:27 -0500

    Logger comments/capitalization (#61)
    Methods in Logger were commented, and method names had their first character capitalized.

--- Proof Productions/Controller/Logger.cs
+++ Proof Productions/Controller/Logger.cs
@@ -4,25 +4,36 @@ namespace Proof_Productions.Controller
 {
     class Logger
     {
-
-        // The log files get saved to my documents
+        ///<summary>
+        ///This finds the log folder in "MyDocuments" to store the Log file in.
+        ///</summary>
         public static String GetLogFolder()
         {
             return Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\proofProductionLogs\\";
         }
-        
-        // The files are named log-date
-        // Ex: log-12152017
+      
+        /// <summary>
+        /// This sets the name of the Log file to "log-MMddyyyy.txt", where MMddyyyy
+        /// is the date at which the Log was created
+        /// </summary>
         public static String GetLogPath()
         {
             return GetLogFolder() + "log-" + DateTime.Now.ToString("MMddyyyy") + ".txt";
         }
 
+        /// <summary>
+        /// If no Log folder exists in "MyDocuments", then a new folder is created.
+        /// </summary>
         public static void MakeLogFolderIfDoesntExist()
         {
             System.IO.Directory.CreateDirectory(GetLogFolder());
         }
 
+        /// <summary>
+        /// This applies the current time to the Log file,
+        /// followed by the Log's message itself when sending.
+        /// It then automatically goes to the next line for the next Log.
+        /// </summary>
         public static void LogInfo(String message)
         {
             MakeLogFolderIfDoesntExist();
@@ -31,6 +42,11 @@ namespace Proof_Productions.Controller
             System.IO.File.AppendAllText(GetLogPath(), System.Environment.NewLine);
         }
 
+        /// <summary>
+        /// When an error occurs, the time is printed into the log followed by a declaration of error.
+        /// Then the message of why there is an error is printed.
+        /// Followed by a new line for the next Log.
+        /// </summary>
         public static void LogError(String message)
         {
             MakeLogFolderIfDoesntExist();
@@ -39,11 +55,17 @@ namespace Proof_Productions.Controller
             System.IO.File.AppendAllText(GetLogPath(), System.Environment.NewLine);
         }
 
+        /// <summary>
+        /// This will send the current date and time when called, used in Log entries.
+        /// </summary>
         private static String CurrentTime()
         {
             return DateTime.Now.ToString("MM/dd/yyyy - h:mm:ss tt");
         }
 
+        /// <summary>
+        /// This turns Log messages into a single line instead of moving to a new line.
+        /// </summary>
         private static String FormatMessage(String message)
         {
             return message.Replace(System.Environment.NewLine, " ");
