d5552fcaf572644fda97ff3097b127e10666c2b4
stephencanzanese <stephen.canzanese@gmail.com>
2017-12-15 13:35:14 -0500

    Fix resizing (#63)

--- Proof Productions/View/NewCueItemForm.Designer.cs
+++ Proof Productions/View/NewCueItemForm.Designer.cs
@@ -60,145 +60,162 @@
             // CueItemNumber
             // 
             this.CueItemNumber.AutoSize = true;
-            this.CueItemNumber.Location = new System.Drawing.Point(22, 68);
+            this.CueItemNumber.Location = new System.Drawing.Point(29, 84);
+            this.CueItemNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CueItemNumber.Name = "CueItemNumber";
-            this.CueItemNumber.Size = new System.Drawing.Size(89, 13);
+            this.CueItemNumber.Size = new System.Drawing.Size(117, 17);
             this.CueItemNumber.TabIndex = 0;
             this.CueItemNumber.Text = "Cue Item Number";
             // 
             // StartDelayLabel
             // 
             this.StartDelayLabel.AutoSize = true;
-            this.StartDelayLabel.Location = new System.Drawing.Point(22, 95);
+            this.StartDelayLabel.Location = new System.Drawing.Point(29, 117);
+            this.StartDelayLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.StartDelayLabel.Name = "StartDelayLabel";
-            this.StartDelayLabel.Size = new System.Drawing.Size(59, 13);
+            this.StartDelayLabel.Size = new System.Drawing.Size(78, 17);
             this.StartDelayLabel.TabIndex = 1;
             this.StartDelayLabel.Text = "Start Delay";
             // 
             // DurationLabel
             // 
             this.DurationLabel.AutoSize = true;
-            this.DurationLabel.Location = new System.Drawing.Point(22, 121);
+            this.DurationLabel.Location = new System.Drawing.Point(29, 149);
+            this.DurationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DurationLabel.Name = "DurationLabel";
-            this.DurationLabel.Size = new System.Drawing.Size(47, 13);
+            this.DurationLabel.Size = new System.Drawing.Size(62, 17);
             this.DurationLabel.TabIndex = 2;
             this.DurationLabel.Text = "Duration";
             // 
             // VelocityLabel
             // 
             this.VelocityLabel.AutoSize = true;
-            this.VelocityLabel.Location = new System.Drawing.Point(257, 42);
+            this.VelocityLabel.Location = new System.Drawing.Point(343, 52);
+            this.VelocityLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.VelocityLabel.Name = "VelocityLabel";
-            this.VelocityLabel.Size = new System.Drawing.Size(44, 13);
+            this.VelocityLabel.Size = new System.Drawing.Size(57, 17);
             this.VelocityLabel.TabIndex = 3;
             this.VelocityLabel.Text = "Velocity";
             // 
             // AccelLabel
             // 
             this.AccelLabel.AutoSize = true;
-            this.AccelLabel.Location = new System.Drawing.Point(257, 68);
+            this.AccelLabel.Location = new System.Drawing.Point(343, 84);
+            this.AccelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.AccelLabel.Name = "AccelLabel";
-            this.AccelLabel.Size = new System.Drawing.Size(66, 13);
+            this.AccelLabel.Size = new System.Drawing.Size(86, 17);
             this.AccelLabel.TabIndex = 4;
             this.AccelLabel.Text = "Acceleration";
             // 
             // DecelLabel
             // 
             this.DecelLabel.AutoSize = true;
-            this.DecelLabel.Location = new System.Drawing.Point(258, 95);
+            this.DecelLabel.Location = new System.Drawing.Point(344, 117);
+            this.DecelLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DecelLabel.Name = "DecelLabel";
-            this.DecelLabel.Size = new System.Drawing.Size(67, 13);
+            this.DecelLabel.Size = new System.Drawing.Size(88, 17);
             this.DecelLabel.TabIndex = 5;
             this.DecelLabel.Text = "Deceleration";
             // 
             // MotorLabel
             // 
             this.MotorLabel.AutoSize = true;
-            this.MotorLabel.Location = new System.Drawing.Point(453, 68);
+            this.MotorLabel.Location = new System.Drawing.Point(604, 84);
+            this.MotorLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.MotorLabel.Name = "MotorLabel";
-            this.MotorLabel.Size = new System.Drawing.Size(65, 13);
+            this.MotorLabel.Size = new System.Drawing.Size(85, 17);
             this.MotorLabel.TabIndex = 9;
             this.MotorLabel.Text = "Motor Name";
             // 
             // CueLabel
             // 
             this.CueLabel.AutoSize = true;
-            this.CueLabel.Location = new System.Drawing.Point(453, 42);
+            this.CueLabel.Location = new System.Drawing.Point(604, 52);
+            this.CueLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CueLabel.Name = "CueLabel";
-            this.CueLabel.Size = new System.Drawing.Size(57, 13);
+            this.CueLabel.Size = new System.Drawing.Size(74, 17);
             this.CueLabel.TabIndex = 10;
             this.CueLabel.Text = "Cue Name";
             // 
             // CueItemNumberTextBox
             // 
-            this.CueItemNumberTextBox.Location = new System.Drawing.Point(117, 65);
+            this.CueItemNumberTextBox.Location = new System.Drawing.Point(156, 80);
+            this.CueItemNumberTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CueItemNumberTextBox.Name = "CueItemNumberTextBox";
-            this.CueItemNumberTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CueItemNumberTextBox.Size = new System.Drawing.Size(132, 23);
             this.CueItemNumberTextBox.TabIndex = 11;
             this.CueItemNumberTextBox.TextChanged += new System.EventHandler(this.CueItemNumberTextBox_TextChanged);
             this.CueItemNumberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
             // 
             // DelayTextbox
             // 
-            this.DelayTextbox.Location = new System.Drawing.Point(117, 92);
+            this.DelayTextbox.Location = new System.Drawing.Point(156, 113);
+            this.DelayTextbox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DelayTextbox.Name = "DelayTextbox";
-            this.DelayTextbox.Size = new System.Drawing.Size(100, 20);
+            this.DelayTextbox.Size = new System.Drawing.Size(132, 23);
             this.DelayTextbox.TabIndex = 12;
             this.DelayTextbox.TextChanged += new System.EventHandler(this.DelayTextbox_TextChanged);
             this.DelayTextbox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
             // 
             // DurationTextBox
             // 
-            this.DurationTextBox.Location = new System.Drawing.Point(117, 118);
+            this.DurationTextBox.Location = new System.Drawing.Point(156, 145);
+            this.DurationTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DurationTextBox.Name = "DurationTextBox";
-            this.DurationTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DurationTextBox.Size = new System.Drawing.Size(132, 23);
             this.DurationTextBox.TabIndex = 13;
             this.DurationTextBox.TextChanged += new System.EventHandler(this.DurationTextBox_TextChanged);
             this.DurationTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
             // 
             // VelocityTextBox
             // 
-            this.VelocityTextBox.Location = new System.Drawing.Point(329, 39);
+            this.VelocityTextBox.Location = new System.Drawing.Point(439, 48);
+            this.VelocityTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.VelocityTextBox.Name = "VelocityTextBox";
-            this.VelocityTextBox.Size = new System.Drawing.Size(100, 20);
+            this.VelocityTextBox.Size = new System.Drawing.Size(132, 23);
             this.VelocityTextBox.TabIndex = 14;
             this.VelocityTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
             // 
             // AccelTextBox
             // 
-            this.AccelTextBox.Location = new System.Drawing.Point(329, 65);
+            this.AccelTextBox.Location = new System.Drawing.Point(439, 80);
+            this.AccelTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.AccelTextBox.Name = "AccelTextBox";
-            this.AccelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.AccelTextBox.Size = new System.Drawing.Size(132, 23);
             this.AccelTextBox.TabIndex = 15;
             this.AccelTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
             // 
             // DecelTextBox
             // 
-            this.DecelTextBox.Location = new System.Drawing.Point(329, 92);
+            this.DecelTextBox.Location = new System.Drawing.Point(439, 113);
+            this.DecelTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DecelTextBox.Name = "DecelTextBox";
-            this.DecelTextBox.Size = new System.Drawing.Size(100, 20);
+            this.DecelTextBox.Size = new System.Drawing.Size(132, 23);
             this.DecelTextBox.TabIndex = 16;
             this.DecelTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
             // 
             // CueTextBox
             // 
-            this.CueTextBox.Location = new System.Drawing.Point(524, 39);
+            this.CueTextBox.Location = new System.Drawing.Point(699, 48);
+            this.CueTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CueTextBox.Name = "CueTextBox";
-            this.CueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.CueTextBox.Size = new System.Drawing.Size(132, 23);
             this.CueTextBox.TabIndex = 20;
             // 
             // MotorTextBox
             // 
-            this.MotorTextBox.Location = new System.Drawing.Point(524, 65);
+            this.MotorTextBox.Location = new System.Drawing.Point(699, 80);
+            this.MotorTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MotorTextBox.Name = "MotorTextBox";
-            this.MotorTextBox.Size = new System.Drawing.Size(100, 20);
+            this.MotorTextBox.Size = new System.Drawing.Size(132, 23);
             this.MotorTextBox.TabIndex = 21;
             // 
-            // CancelButton
+            // CancelCreationButton
             // 
-            this.CancelCreationButton.Location = new System.Drawing.Point(354, 228);
-            this.CancelCreationButton.Name = "CancelButton";
-            this.CancelCreationButton.Size = new System.Drawing.Size(75, 23);
+            this.CancelCreationButton.Location = new System.Drawing.Point(472, 281);
+            this.CancelCreationButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CancelCreationButton.Name = "CancelCreationButton";
+            this.CancelCreationButton.Size = new System.Drawing.Size(100, 28);
             this.CancelCreationButton.TabIndex = 25;
             this.CancelCreationButton.Text = "Cancel";
             this.CancelCreationButton.UseVisualStyleBackColor = true;
@@ -208,17 +225,19 @@
             // 
             this.panel1.Controls.Add(this.CCWRadioButton);
             this.panel1.Controls.Add(this.CWRadioButton);
-            this.panel1.Location = new System.Drawing.Point(479, 108);
+            this.panel1.Location = new System.Drawing.Point(639, 133);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(267, 123);
             this.panel1.TabIndex = 26;
             // 
             // CCWRadioButton
             // 
             this.CCWRadioButton.AutoSize = true;
-            this.CCWRadioButton.Location = new System.Drawing.Point(3, 26);
+            this.CCWRadioButton.Location = new System.Drawing.Point(4, 32);
+            this.CCWRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CCWRadioButton.Name = "CCWRadioButton";
-            this.CCWRadioButton.Size = new System.Drawing.Size(113, 17);
+            this.CCWRadioButton.Size = new System.Drawing.Size(141, 21);
             this.CCWRadioButton.TabIndex = 26;
             this.CCWRadioButton.Text = "Counter Clockwise";
             this.CCWRadioButton.UseVisualStyleBackColor = true;
@@ -227,9 +246,10 @@
             // 
             this.CWRadioButton.AutoSize = true;
             this.CWRadioButton.Checked = true;
-            this.CWRadioButton.Location = new System.Drawing.Point(3, 3);
+            this.CWRadioButton.Location = new System.Drawing.Point(4, 4);
+            this.CWRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CWRadioButton.Name = "CWRadioButton";
-            this.CWRadioButton.Size = new System.Drawing.Size(73, 17);
+            this.CWRadioButton.Size = new System.Drawing.Size(87, 21);
             this.CWRadioButton.TabIndex = 26;
             this.CWRadioButton.TabStop = true;
             this.CWRadioButton.Text = "Clockwise";
@@ -238,26 +258,28 @@
             // PositionLabel
             // 
             this.PositionLabel.AutoSize = true;
-            this.PositionLabel.Location = new System.Drawing.Point(258, 121);
+            this.PositionLabel.Location = new System.Drawing.Point(344, 149);
+            this.PositionLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PositionLabel.Name = "PositionLabel";
-            this.PositionLabel.Size = new System.Drawing.Size(44, 13);
+            this.PositionLabel.Size = new System.Drawing.Size(58, 17);
             this.PositionLabel.TabIndex = 25;
             this.PositionLabel.Text = "Position";
             // 
             // PositionTextBox
             // 
-            this.PositionTextBox.Location = new System.Drawing.Point(329, 118);
+            this.PositionTextBox.Location = new System.Drawing.Point(439, 145);
+            this.PositionTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PositionTextBox.Name = "PositionTextBox";
-            this.PositionTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PositionTextBox.Size = new System.Drawing.Size(132, 23);
             this.PositionTextBox.TabIndex = 17;
             this.PositionTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberOnlyTextBox_KeyPress);
-
             // 
             // SubmitButton
             // 
-            this.SubmitButton.Location = new System.Drawing.Point(248, 228);
+            this.SubmitButton.Location = new System.Drawing.Point(331, 281);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SubmitButton.Name = "SubmitButton";
-            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.Size = new System.Drawing.Size(100, 28);
             this.SubmitButton.TabIndex = 24;
             this.SubmitButton.Text = "Submit";
             this.SubmitButton.UseVisualStyleBackColor = true;
@@ -266,24 +288,26 @@
             // CueItemNameTextBox
             // 
             this.CueItemNameTextBox.AutoSize = true;
-            this.CueItemNameTextBox.Location = new System.Drawing.Point(22, 42);
+            this.CueItemNameTextBox.Location = new System.Drawing.Point(29, 52);
+            this.CueItemNameTextBox.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CueItemNameTextBox.Name = "CueItemNameTextBox";
-            this.CueItemNameTextBox.Size = new System.Drawing.Size(80, 13);
+            this.CueItemNameTextBox.Size = new System.Drawing.Size(104, 17);
             this.CueItemNameTextBox.TabIndex = 28;
             this.CueItemNameTextBox.Text = "Cue Item Name";
             // 
             // NameTextBox
             // 
-            this.NameTextBox.Location = new System.Drawing.Point(117, 39);
+            this.NameTextBox.Location = new System.Drawing.Point(156, 48);
+            this.NameTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.NameTextBox.Name = "NameTextBox";
-            this.NameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.NameTextBox.Size = new System.Drawing.Size(132, 23);
             this.NameTextBox.TabIndex = 0;
             // 
             // NewCueItemForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(641, 277);
+            this.ClientSize = new System.Drawing.Size(855, 341);
             this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.CueItemNameTextBox);
             this.Controls.Add(this.SubmitButton);
@@ -307,6 +331,10 @@
             this.Controls.Add(this.DurationLabel);
             this.Controls.Add(this.StartDelayLabel);
             this.Controls.Add(this.CueItemNumber);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(871, 380);
+            this.MinimumSize = new System.Drawing.Size(871, 380);
             this.Name = "NewCueItemForm";
             this.Text = "NewCueItemForm";
             this.panel1.ResumeLayout(false);
